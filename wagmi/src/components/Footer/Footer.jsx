7760e328fcde71f7638f801158e3aa287dca1315
arunnats <arun_b220019cs@nitc.ac.in>
2024-09-09 01:01:14 +0530

    ned to make footer more repsonsive

--- wagmi/src/components/Footer/Footer.jsx
+++ wagmi/src/components/Footer/Footer.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import wagmiIcon from "../../assets/images/wagmiIcon.svg";
-import { FaFacebookF, FaTwitter, FaInstagram } from "react-icons/fa"; // Example icons
+import { FaFacebookF, FaTwitter, FaInstagram } from "react-icons/fa";
 
 const Footer = () => {
 	return (
-		<div className="flex justify-around items-center h-[250px] bg-wagmi-blue relative mt-[12vh] md:mt-[14vh] lg:mt-[18vh] px-8">
+		<div className="flex flex-col md:flex-row justify-around items-center h-auto md:h-[250px] bg-wagmi-blue relative mt-[12vh] md:mt-[14vh] lg:mt-[18vh] px-8 py-8">
 			{/* Google Map on the left */}
-			<div className="w-[20vw] h-[30vh]  ">
+			<div className="w-full md:w-[30vw] h-[200px] md:h-[30vh] mb-6 md:mb-0">
 				<iframe
 					src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31437.43213844723!2d76.20789489389806!3d9.960641200487823!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b086d314f0b178d%3A0xc545233f390db43b!2sFort%20Kochi%2C%20Kochi%2C%20Kerala!5e0!3m2!1sen!2sin!4v1725822611924!5m2!1sen!2sin"
 					width="100%"
@@ -19,16 +19,16 @@ const Footer = () => {
 			</div>
 
 			{/* Center area for icon, address, and phone number */}
-			<div className="w-[33vw] h-[200px]">
+			<div className="w-full md:w-[33vw] h-auto md:h-[200px] mb-6 md:mb-0">
 				<div className="text-center flex flex-col items-center">
 					<div className="absolute -translate-y-24 w-36 h-36 shadow-[0px_7px_24.5px_rgba(0,_0,_0,_0.25)] rounded-full bg-wagmi-blue flex justify-center items-center">
 						<img className="w-16 h-16" src={wagmiIcon} alt="WAGMI Icon" />
 					</div>
 					<div className="mt-24">
-						<p className="text-wagmi-white font-semibold font-gilmer">
+						<p className="text-wagmi-white font-semibold font-gilmer text-sm sm:text-base lg:text-lg">
 							123 Wagmi St, Kochi, Kerala
 						</p>
-						<p className="text-wagmi-white font-semibold font-gilmer">
+						<p className="text-wagmi-white font-semibold font-gilmer text-sm sm:text-base lg:text-lg">
 							Phone: +91 12345 67890
 						</p>
 					</div>
@@ -36,16 +36,34 @@ const Footer = () => {
 			</div>
 
 			{/* Social media icons on the right */}
-			<div className="w-[20vw] h-[30vh] ">
-				<div className="flex flex-col justify-center text-wagmi-white">
-					<a href="#" aria-label="Facebook">
+			<div className="w-full md:w-[30vw] h-auto md:h-[30vh] flex justify-center">
+				<div className="flex flex-col md:flex-row items-center text-wagmi-white font-gilmer space-y-4 md:space-y-0 md:space-x-8">
+					{/* Facebook */}
+					<a
+						href="#"
+						className="flex items-center space-x-2"
+						aria-label="Facebook"
+					>
 						<FaFacebookF size={30} />
+						<span>@wagmi</span>
 					</a>
-					<a href="#" aria-label="Twitter">
+					{/* Twitter */}
+					<a
+						href="#"
+						className="flex items-center space-x-2"
+						aria-label="Twitter"
+					>
 						<FaTwitter size={30} />
+						<span>@wagmi2</span>
 					</a>
-					<a href="#" aria-label="Instagram">
+					{/* Instagram */}
+					<a
+						href="#"
+						className="flex items-center space-x-2"
+						aria-label="Instagram"
+					>
 						<FaInstagram size={30} />
+						<span>@wagmi</span>
 					</a>
 				</div>
 			</div>
