79af71bb81655321f59da6682a9e8bf7d59f972d
arunnats <arun_b220019cs@nitc.ac.in>
2024-09-09 00:03:49 +0530

    Update Footer.jsx

--- wagmi/src/components/Footer/Footer.jsx
+++ wagmi/src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import wagmiIcon from "../.././assets/images/wagmiIcon.svg";
 
 const Footer = () => {
 	return (
-		<div className="flex justify-center h-[250px] bg-wagmi-blue relative mt-[18vh]">
+		<div className="flex justify-center h-[250px] bg-wagmi-blue relative mt-[12vh] md:mt-[14vh] lg:mt-[18vh]">
 			<div className="absolute -translate-y-16 w-36 h-36 shadow-[0px_7px_24.5px_rgba(0,_0,_0,_0.25)] rounded-full bg-wagmi-blue flex justify-center items-center">
 				<img className="w-16 h-16 " src={wagmiIcon} alt="WAGMI Icon" />
 			</div>
