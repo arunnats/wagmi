26adcca94b9e30ba83379bd451cf71966bb883c8
arunnats <arun_b220019cs@nitc.ac.in>
2024-10-11 13:25:48 +0530

    Update Card.jsx

--- wagmi/src/components/Card/Card.jsx
+++ wagmi/src/components/Card/Card.jsx
@@ -6,14 +6,14 @@ const CardComponent = ({ title, content }) => {
 		<div className="relative w-[38vw] h-[39.6108vw] md:w-[19.18665vw] md:h-[20.7vw] rounded-[14px] overflow-hidden bg-wagmi-blue p-[.3vw] flex flex-col justify-start transition-transform duration-300 ease-out hover:-translate-y-[5px]">
 			{/* Title Section */}
 			<div className="flex flex-col items-center">
-				<h2 className="text-wagmi-white text-[4.5vw] md:text-[1.1rem] lg:text-[1.5rem] xl:text-[1.9rem] font-gilmerbold text-center mt-1">
+				<h2 className="text-wagmi-white text-[3.6vw] md:text-[1.1rem] lg:text-[1.5rem] xl:text-[1.9rem] font-gilmerbold text-center mt-3 md:mt-2">
 					{title}
 				</h2>
 			</div>
 
 			{/* Content Section */}
 			<div className="flex-grow flex items-start justify-center w-[95%]">
-				<p className="text-black text-[3.3vw] md:text-[1.55vw] lg:text-[1.55vw] xl:text-[1.5vw] 2xl:text-[1.5vw] tracking-tighter font-gilmer text-wrap px-[.6vw] md:px-[.6vw] 2xl:px-[2vw]">
+				<p className="text-black text-[2.8vw] md:text-[1.55vw] lg:text-[1.55vw] xl:text-[1.5vw] 2xl:text-[1.5vw] tracking-tighter font-gilmer text-wrap px-[2vw] md:px-[.6vw] 2xl:px-[2vw]">
 					{content}
 				</p>
 			</div>
