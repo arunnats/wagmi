0578a51f4b5bd7aca7fb6e549b4df090642bcc53
arunnats <arun_b220019cs@nitc.ac.in>
2024-10-11 13:21:42 +0530

    Update Carousel.jsx

--- wagmi/src/components/Content/Carousel.jsx
+++ wagmi/src/components/Content/Carousel.jsx
@@ -9,47 +9,47 @@ import six from "../../assets/images/OurSpaces/6.webp";
 
 const Carousel = () => {
 	return (
-		<div className="flex justify-center mt-8 w-[80vw] h-[55vh] mx-auto bg-wagmi-offwhite border-wagmi-blue rounded-box border-[2px] md:border-[4px] border-solid">
+		<div className="flex justify-center items-center mt-8 w-[80vw] h-[35vh] sm:h-[40vh] md:h-[40vh] lg:h-[55vh] mx-auto bg-wagmi-offwhite border-wagmi-blue rounded-box border-[2px] md:border-[4px] border-solid">
 			<div className="rounded-box px-3 md:px-6 md:p-6">
 				<CCarousel controls>
 					<CCarouselItem>
 						<CImage
-							className="d-block max-h-[45vh] mx-auto aspect-[16/9] object-cover"
+							className="d-block max-h-[45vh] mx-auto my-auto aspect-[16/9] object-cover"
 							src={one}
 							alt="slide 1"
 						/>
 					</CCarouselItem>
 					<CCarouselItem>
 						<CImage
-							className="d-block max-h-[45vh] mx-auto aspect-[16/9] object-cover"
+							className="d-block max-h-[45vh] mx-auto my-auto aspect-[16/9] object-cover"
 							src={two}
 							alt="slide 2"
 						/>
 					</CCarouselItem>
 					<CCarouselItem>
 						<CImage
-							className="d-block max-h-[45vh] mx-auto aspect-[16/9] object-cover"
+							className="d-block max-h-[45vh] mx-auto my-auto aspect-[16/9] object-cover"
 							src={three}
 							alt="slide 3"
 						/>
 					</CCarouselItem>
 					<CCarouselItem>
 						<CImage
-							className="d-block max-h-[45vh] mx-auto aspect-[16/9] object-cover"
+							className="d-block max-h-[45vh] mx-auto my-auto aspect-[16/9] object-cover"
 							src={four}
 							alt="slide 4"
 						/>
 					</CCarouselItem>
 					<CCarouselItem>
 						<CImage
-							className="d-block max-h-[45vh] mx-auto aspect-[16/9] object-cover"
+							className="d-block max-h-[45vh] mx-auto my-auto aspect-[16/9] object-cover"
 							src={five}
 							alt="slide 5"
 						/>
 					</CCarouselItem>
 					<CCarouselItem>
 						<CImage
-							className="d-block w-full mx-auto max-h-[45vh] aspect-[16/9] object-cover"
+							className="d-block max-h-[45vh] mx-auto my-auto aspect-[16/9] object-cover"
 							src={six}
 							alt="slide 6"
 						/>
